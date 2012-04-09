e200a29ca2b16a0c5b7467fbdcc62e00936b638e
Vladimir Adamić <vladimir.adamic@gmail.com>
2012-04-09 22:15:15 +0200

    added tranlsation files, need configuring

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :surname, :email, :password, :password_confirmation
   has_secure_password
 
-  validates :name, presence: true, length: {maximum: 50} 
+  validates :name, presence: true, length: {maximum: 50}
   validates :surname, presence: true, length: {maximum: 100}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
