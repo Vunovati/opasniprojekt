03bb4d1ac4451c10881f08c9ca5a21936afae804
Vidreven <vidreven@yahoo.com>
2012-04-14 14:59:19 +0200

    admin added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120408161518) do
+ActiveRecord::Schema.define(:version => 20120414125157) do
 
   create_table "accomodations", :force => true do |t|
     t.string   "property_type"
@@ -38,10 +38,11 @@ ActiveRecord::Schema.define(:version => 20120408161518) do
     t.string   "name"
     t.string   "surname"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
